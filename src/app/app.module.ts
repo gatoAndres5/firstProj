1059d6072ba6e5560b222fc80301fb4ff0de4c59
gatoAndres5 <117231128+gatoAndres5@users.noreply.github.com>
2023-05-19 00:45:46 -0700

    new2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
+//import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HousingLocationComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+//@NgModule({
+  //declarations: [
+    //AppComponent,
+    //HomeComponent,
+    //HousingLocationComponent
+ // ],
+  //imports: [
+ //   BrowserModule
+  //],
+ // providers: [],
+ // bootstrap: [AppComponent]
+//})
+//export class AppModule { }
