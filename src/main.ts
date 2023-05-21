6722f5ce941c4d8e2adb62a28bf37747f259cf55
gatoAndres5 <andgalv@arizona.edu>
2023-05-20 22:12:54 -0700

    Lesson 10 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 //import { AppModule } from './app/app.module';
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import routeConfig from './app/routes';
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
+    {providers: [provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+    })
   .catch(err => console.error(err));
 
 //platformBrowserDynamic().bootstrapModule(module)
